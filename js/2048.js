c1cb4617b15a2af3457bfd0c1ac587c745fa1617
fifthwolf <fifthwolf@qq.com>
2017-09-10 23:01:08 +0800

    add demo 2048

--- js/2048.js
+++ js/2048.js
@@ -1,1 +1,33 @@
 var content = document.getElementsByClassName('content')[0];
+var cell = content.getElementsByClassName('cell')[0];
+var changeButton = document.getElementById('generate');
+
+window.onload = function () {
+  createCell(4);
+}
+
+function createCell (length) {
+  cell.innerHTML = '';
+  var row = new Array(length);
+  for (var i = 0; i < length; i++) {
+    row[i] = document.createElement('div');
+    for (var j = 0; j < length; j++) {
+      var col = new Array(length);
+      col[j] = document.createElement('span');
+      row[i].appendChild(col[j]);
+    }
+    cell.appendChild(row[i]);
+  }
+  cell.style.width = length * 60 + 'px';
+}
+
+addEvent (changeButton, 'click', function () {
+  var length = Number(document.getElementById('length').value);
+  if(length >= 4 && length <= 8 && _isInteger(length)){
+    createCell(length);
+  }
+  function _isInteger (obj) {
+    return typeof obj === 'number' && obj % 1 === 0;
+  }
+});
+
