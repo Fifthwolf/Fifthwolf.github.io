3c6c13e1cbcfd5aaad87fd61977fb0ff3f517031
fifthwolf <fifthwolf@qq.com>
2017-09-11 12:38:38 +0800

    add demo 2048 js

--- js/2048.js
+++ js/2048.js
@@ -4,7 +4,15 @@ var changeButton = document.getElementById('change');
 
 var length = 4;
 
+var data = {
+  value:[]
+}
+
 window.onload = function () {
+  data.value[0] = '&nbsp;';
+  for (var i = 1; i < 16; i++) {
+    data.value[i] = Math.pow(2, i);
+  }
   createCell(length);
   starGame(length);
 }
@@ -44,24 +52,25 @@ function starGame (length) {
     box[i] = row[i].getElementsByTagName('span');
   }
 
-  //初始化格子
-  initializeBox(box, length);
-
   addEvent (document, 'keydown', function (e) {
     var keynum = window.event ? e.keyCode : e.which;
     switch (keynum) {
-    //左
-    case 37: moveBoxLeft(box, length); break;
-    //上
-    case 38: console.log(38); break;
-    //右
-    case 39: console.log(39); break;
-    //下
-    case 40: console.log(40); break;
-  }
-});
+      //左
+      case 37: moveBoxLeft(box, length); break;
+      //上
+      case 38: moveBoxTop(box, length); break;
+      //右
+      case 39: console.log(39); break;
+      //下
+      case 40: console.log(40); break;
+    }
+  });
+
+  //增加格子
+  initializeBox(box, length);
 }
 
+//初始化格子
 function initializeBox (box, length) {
   if (fail(box, length)) {
     return;
@@ -70,14 +79,17 @@ function initializeBox (box, length) {
     var x = parseInt(Math.random() * 4);
     var y = parseInt(Math.random() * 4);
     if (box[x][y].getAttribute('boxType') === '0') {
-      box[x][y].innerHTML = '2';
+      box[x][y].innerHTML = data.value[1];
       box[x][y].setAttribute('boxType','1');
+      box[x][y].setAttribute('class', 'type1');
       i--;
     }
   }
 }
 
+//向左移动
 function moveBoxLeft (box, length) {
+  mergeBoxLeft(box, length);
   for (var i = 0; i < length; i++) {
     var empty = 0;
     for (var j = 0; j < length; j++) {
@@ -86,8 +98,10 @@ function moveBoxLeft (box, length) {
         tempAttribute = boxType;
         tempInnerHTML = box[i][j].innerHTML;   
         box[i][j].setAttribute('boxType', '0');
+        box[i][j].setAttribute('class', '');
         box[i][j].innerHTML = '&nbsp;';
         box[i][empty].setAttribute('boxType', tempAttribute);
+        box[i][empty].setAttribute('class', 'type' + tempAttribute);
         box[i][empty].innerHTML = tempInnerHTML;
         empty++;
       }
@@ -96,9 +110,95 @@ function moveBoxLeft (box, length) {
   initializeBox(box, length);
 }
 
-function merge (box, length) {
+//向左合并
+function mergeBoxLeft (box, length) {
   for (var i = 0; i < length; i++) {
+    var boxPre, boxTypeTemp;
+    var merge = 0;
     for (var j = 0; j < length; j++) {
+      var boxType = box[i][j].getAttribute('boxType');
+      if (boxType === '0') {
+        continue;
+      } else {
+        if (merge === 0) {
+          boxPre = box[i][j];
+          boxPreType = boxPre.getAttribute('boxType');
+          merge = 1;
+          continue;
+        }
+        if (merge === 1) {
+          if (boxPreType === boxType) {
+            boxPre.setAttribute('boxType', parseInt(boxType) + 1);
+            boxPre.setAttribute('class', 'type' + parseInt(boxType) + 1);
+            boxPre.innerHTML = data.value[parseInt(boxType) + 1];
+            box[i][j].setAttribute('boxType', '0');
+            box[i][j].setAttribute('class', '');
+            box[i][j].innerHTML = '&nbsp;';
+            merge = 0;
+          } else {
+            boxPre = box[i][j];
+            boxPreType = boxPre.getAttribute('boxType');
+          }  
+        }
+      }
+    }
+  }
+}
+
+//向上移动
+function moveBoxTop (box, length) {
+  mergeBoxTop(box, length);
+  for (var i = 0; i < length; i++) {
+    var empty = 0;
+    for (var j = 0; j < length; j++) {
+      var boxType = box[j][i].getAttribute('boxType');
+      if (boxType !== '0') {
+        tempAttribute = boxType;
+        tempInnerHTML = box[j][i].innerHTML;   
+        box[j][i].setAttribute('boxType', '0');
+        box[j][i].setAttribute('class', '');
+        box[j][i].innerHTML = '&nbsp;';
+        box[empty][i].setAttribute('boxType', tempAttribute);
+        box[empty][i].setAttribute('class', 'type' + tempAttribute);
+        box[empty][i].innerHTML = tempInnerHTML;
+        empty++;
+      }
+    }
+  }
+  initializeBox(box, length);
+}
+
+//向上合并
+function mergeBoxTop (box, length) {
+  for (var i = 0; i < length; i++) {
+    var boxPre, boxTypeTemp;
+    var merge = 0;
+    for (var j = 0; j < length; j++) {
+      var boxType = box[j][i].getAttribute('boxType');
+      if (boxType === '0') {
+        continue;
+      } else {
+        if (merge === 0) {
+          boxPre = box[j][i];
+          boxPreType = boxPre.getAttribute('boxType');
+          merge = 1;
+          continue;
+        }
+        if (merge === 1) {
+          if (boxPreType === boxType) {
+            boxPre.setAttribute('boxType', parseInt(boxType) + 1);
+            boxPre.setAttribute('class', 'type' + parseInt(boxType) + 1);
+            boxPre.innerHTML = data.value[parseInt(boxType) + 1];
+            box[j][i].setAttribute('boxType', '0');
+            box[j][i].setAttribute('class', '');
+            box[j][i].innerHTML = '&nbsp;';
+            merge = 0;
+          } else {
+            boxPre = box[j][i];
+            boxPreType = boxPre.getAttribute('boxType');
+          }  
+        }
+      }
     }
   }
 }
