4638dec498a436ea4ce3299a85f7585fda5ebe5a
fifthwolf <fifthwolf@qq.com>
2017-09-17 22:13:01 +0800

    add demo linkUp

---
+++ js/linkUp.js
@@ -0,0 +1,23 @@
+﻿var content = document.getElementsByClassName('content')[0];
+var mainView = content.getElementsByClassName('mainView')[0];
+
+window.onload = function () {
+  delayedLoadingPublicPictures ('../');
+}
+
+data = {
+  row:8,
+  col:14,
+  box:[]
+}
+
+for (var i = 0; i < data.row; i++) {
+  var row = new Array(data.row);
+  row[i] = document.createElement('div');
+  for (var j = 0; j < data.col; j++) {
+    var col = new Array(data.col);
+    col[j] = document.createElement('span');
+    row[i].appendChild(col[j]);
+  }
+  mainView.appendChild(row[i]);
+}
