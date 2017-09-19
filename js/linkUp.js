9742541c3928605807b93923f3e4aa83346289b9
fifthwolf <fifthwolf@qq.com>
2017-09-19 18:42:15 +0800

    update demo html

--- js/linkUp.js
+++ js/linkUp.js
@@ -11,12 +11,14 @@ data = {
   box:[]
 }
 
+//创建DIV、SPAN显示元素
 for (var i = 0; i < data.row; i++) {
   var row = new Array(data.row);
   row[i] = document.createElement('div');
   for (var j = 0; j < data.col; j++) {
     var col = new Array(data.col);
     col[j] = document.createElement('span');
+    col[j].setAttribute('boxType', '0');
     row[i].appendChild(col[j]);
   }
   mainViewBox.appendChild(row[i]);
