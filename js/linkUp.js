800761aa2fa8fe62df3f8833ac9c7ffbc5f596fa
fifthwolf <fifthwolf@qq.com>
2017-09-20 18:14:10 +0800

    update linkUp

--- js/linkUp.js
+++ js/linkUp.js
@@ -3,23 +3,46 @@ var mainViewBox = content.getElementsByClassName('box')[0];
 
 window.onload = function () {
   delayedLoadingPublicPictures ('../');
+  createFrame();
+  starGame();
 }
 
 data = {
   row:8,
   col:14,
+  type:10,
   box:[]
 }
 
-//创建DIV、SPAN显示元素
-for (var i = 0; i < data.row; i++) {
-  var row = new Array(data.row);
-  row[i] = document.createElement('div');
-  for (var j = 0; j < data.col; j++) {
-    var col = new Array(data.col);
-    col[j] = document.createElement('span');
-    col[j].setAttribute('boxType', '0');
-    row[i].appendChild(col[j]);
+//创建DIV、SPAN 框架元素
+function createFrame () {
+  for (var i = 0; i < data.row; i++) {
+    var row = new Array(data.row);
+    row[i] = document.createElement('div');
+    for (var j = 0; j < data.col; j++) {
+      var col = new Array(data.col);
+      col[j] = document.createElement('span');
+      col[j].setAttribute('boxType', '0');
+      row[i].appendChild(col[j]);
+    }
+    mainViewBox.appendChild(row[i]);
   }
-  mainViewBox.appendChild(row[i]);
+}
+
+function starGame () {
+  var spanElements = mainViewBox.getElementsByTagName('span');
+  var spanArray = new Array();
+  for (var i = 0, len = spanElements.length; i < len; i++) {
+    spanArray[i] = spanElements[i];
+  }
+  while (spanArray.length > 0) {
+    var type = parseInt(Math.random() * data.type + 1);
+    for (var i = 0; i < 2; i++) {
+      var index = parseInt(Math.random() * spanArray.length);
+      spanArray[index].setAttribute('boxType',type);
+      spanArray[index].addClass('type' + type);
+      spanArray[index].innerHTML = type;
+      spanArray.splice(index,1);
+    }   
+  }
 }
