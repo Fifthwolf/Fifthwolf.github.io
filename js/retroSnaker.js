21f521a1852a0be0e4198a3fc78ba68865792a1f
fifthwolf <fifthwolf@qq.com>
2017-10-15 14:28:46 +0800

    update

--- js/retroSnaker.js
+++ js/retroSnaker.js
@@ -16,10 +16,13 @@ var data = {
   start: false,
   box: [],
   direction: 1, //1上2右3下4左
-  snake: []
+  snake: [],
+  eattingPoint: []
 }
 
 addEvent(starButton, 'click', starGame);
+addEvent(document, 'keydown', changeDirection);
+
 
 function createFrame () {
   mainViewBox.innerHTML = '';
@@ -48,25 +51,45 @@ function starGame () {
   data.direction = parseInt(Math.random() * 100 % 4 + 1);
   switch (data.direction) {
     case 1:
-      data.snake.push([data.snake[0][0] + 1, data.snake[0][1]]);
-      break;
+    data.snake.push([data.snake[0][0] + 1, data.snake[0][1]]);
+    data.snake.push([data.snake[0][0] + 2, data.snake[0][1]]);
+    break;
     case 2:
-      data.snake.push([data.snake[0][0], data.snake[0][1] - 1]);
-      break;
+    data.snake.push([data.snake[0][0], data.snake[0][1] - 1]);
+    data.snake.push([data.snake[0][0], data.snake[0][1] - 2]);
+    break;
     case 3:
-      data.snake.push([data.snake[0][0] - 1, data.snake[0][1]]);
-      break;
+    data.snake.push([data.snake[0][0] - 1, data.snake[0][1]]);
+    data.snake.push([data.snake[0][0] - 2, data.snake[0][1]]);
+    break;
     case 4:
-      data.snake.push([data.snake[0][0], data.snake[0][1] + 1]);
-      break;
+    data.snake.push([data.snake[0][0], data.snake[0][1] + 1]);
+    data.snake.push([data.snake[0][0], data.snake[0][1] + 2]);
+    break;
   }
+  createEatPoing();
   draw();
   TIME = setInterval(function () {
     running();
-  }, 500);
+  }, 200);
+}
+
+function changeDirection (e) {
+  var keynum = window.event ? e.keyCode : e.which;
+  switch (keynum) {
+    //左
+    case 37: data.direction = 4; break;
+    //上
+    case 38: data.direction = 1; break;
+    //右
+    case 39: data.direction = 2; break;
+    //下
+    case 40: data.direction = 3; break;
+  }
 }
 
 function running () {
+  var head = [data.snake[0][0], data.snake[0][1]];
   switch (data.direction) {
     case 1:
       data.snake.unshift([data.snake[0][0] - 1, data.snake[0][1]]);
@@ -81,8 +104,27 @@ function running () {
       data.snake.unshift([data.snake[0][0], data.snake[0][1] - 1]);
       break;
   }
-  data.snake.pop();
+  if (head[0] === data.eattingPoint[0] && head[1] === data.eattingPoint[1]) {
+    createEatPoing();
+  } else {
+    data.snake.pop();
+  }
   draw();
+}
+
+function createEatPoing () {
+  do {
+    var flag = false;
+    var row = parseInt(Math.random() * 600 % data.row);
+    var col = parseInt(Math.random() * 600 % data.col);
+    for (var i = 0; i < data.snake.length; i++) {
+      if (data.snake[i][0] === row && data.snake[i][1] === col) {
+        flag = true;
+        break;
+      }
+    }
+  } while ( flag )
+  data.eattingPoint = [row, col];
 }
 
 function draw () {
@@ -96,5 +138,5 @@ function draw () {
   for (var i = 1, len = data.snake.length; i < len; i++) {
     data.box[data.snake[i][0]][data.snake[i][1]].addClass('body');
   }
-  console.log(data.snake);
+  data.box[data.eattingPoint[0]][data.eattingPoint[1]].addClass('eatPoint');
 }
