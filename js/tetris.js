80548e931d564b434a06f14872399235893c59b8
fifthwolf <fifthwolf@qq.com>
2017-10-22 21:30:57 +0800

    update

--- js/tetris.js
+++ js/tetris.js
@@ -1,5 +1,10 @@
 var content = document.getElementsByClassName('content')[0];
 var mainViewBox = content.getElementsByClassName('box')[0];
+var showView = content.getElementsByClassName('showView')[0];
+var nextBox = showView.getElementsByClassName('nextBox')[0];
+var levelBox = document.getElementById('level');
+var scoreBox = document.getElementById('score');
+var pauseBox = showView.getElementsByClassName('pause')[0];
 
 window.onload = function () {
   delayedLoadingPublicPictures ('../');
@@ -8,22 +13,28 @@ window.onload = function () {
 
 data = {
   row: 20,
-  col: 10
+  col: 10,
+  level: 0,
+  score: 0
 }
 
 function createFrame () {
-  mainViewBox.innerHTML = '';
-  data.surplus = data.row * data.col;
-  for (var i = 0; i < data.row; i++) {
-    var row = new Array(data.row);
+  createFrameContext(mainViewBox, data.row, data.col);
+  createFrameContext(nextBox, 2, 4);
+}
+
+function createFrameContext (ele, rowValue, colValue) {
+  ele.innerHTML = '';
+  for (var i = 0; i < rowValue; i++) {
+    var row = new Array(rowValue);
     row[i] = document.createElement('div');
     row[i].setAttribute('row', i);
-    for (var j = 0; j < data.col; j++) {
-      var col = new Array(data.col);
+    for (var j = 0; j < colValue; j++) {
+      var col = new Array(colValue);
       col[j] = document.createElement('span');
       col[j].setAttribute('col', j);
       row[i].appendChild(col[j]);
     }
-    mainViewBox.appendChild(row[i]);
+    ele.appendChild(row[i]);
   }
 }
