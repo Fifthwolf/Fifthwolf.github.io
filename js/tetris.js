816ed381f600845fc420e72e3814183816450ec7
fifthwolf <fifthwolf@qq.com>
2017-10-23 17:23:39 +0800

    update

--- js/tetris.js
+++ js/tetris.js
@@ -1,10 +1,11 @@
 var content = document.getElementsByClassName('content')[0];
-var mainViewBox = content.getElementsByClassName('box')[0];
+var mainBox = document.getElementById('mainBox');
 var showView = content.getElementsByClassName('showView')[0];
 var nextBox = showView.getElementsByClassName('nextBox')[0];
 var levelBox = document.getElementById('level');
 var scoreBox = document.getElementById('score');
 var starButton = document.getElementById('start');
+var TIME;
 
 window.onload = function () {
   delayedLoadingPublicPictures ('../');
@@ -16,13 +17,14 @@ data = {
   col: 10,
   level: 0,
   timeInterval: 1000,
-  score: 0
+  score: 0,
+  box: []
 }
 
 addEvent(starButton, 'click', starGame);
 
 function createFrame () {
-  createFrameContext(mainViewBox, data.row, data.col);
+  createFrameContext(mainBox, data.row, data.col);
   createFrameContext(nextBox, 2, 4);
 }
 
@@ -34,12 +36,23 @@ function initialization () {
   data.level = 1;
   data.timeInterval = 1000;
   data.score = 0;
+  data.box = new Array(data.row);
+  for (var i = 0, currentDivs = mainBox.getElementsByTagName('div'), ilen = currentDivs.length; i < ilen; i++) {
+    data.box[i]  = new Array(data.col);
+    var currentDiv = currentDivs[i];
+    for (var j = 0, currentSpans = currentDiv.getElementsByTagName('span'), jlen = currentSpans.length; j < jlen; j++) {
+      data.box[i][j] = {};
+      data.box[i][j].ele = currentSpans[j];
+      data.box[i][j].type = 0;
+    }
+  }
 }
 
 function createFrameContext (ele, rowValue, colValue) {
   ele.innerHTML = '';
+  var row = new Array(rowValue);
   for (var i = 0; i < rowValue; i++) {
-    var row = new Array(rowValue);
+    data.box[i] = new Array(data.colValue);
     row[i] = document.createElement('div');
     row[i].setAttribute('row', i);
     for (var j = 0; j < colValue; j++) {
