9aef8548908073f866e80e1209af2ddb42208060
fifthwolf <fifthwolf@qq.com>
2017-10-26 20:46:29 +0800

    重构

--- js/tetris.js
+++ js/tetris.js
@@ -3,6 +3,7 @@ var mainBox = document.getElementById('mainBox');
 var showView = content.getElementsByClassName('showView')[0];
 var nextBox = showView.getElementsByClassName('nextBox')[0];
 var levelBox = document.getElementById('level');
+var lineBox = document.getElementById('line');
 var scoreBox = document.getElementById('score');
 var starButton = document.getElementById('start');
 var TIME;
@@ -18,7 +19,6 @@ var data = {
   start: false,
   currentBoxType: 0,
   currentBoxState: 0,
-  currentBoxRotateShift: 0,
   nextBoxType: false,
   dropBox: false,
   level: 0,
@@ -26,25 +26,6 @@ var data = {
   score: 0,
   box: [],
 }
-
-/*
-data.boxType = [
-  [[0,1,1,0],
-  [1,1,0,0]], //S
-  [[1,1,0,0],
-  [0,1,1,0]], //Z
-  [[0,0,1,0],
-  [1,1,1,0]], //L
-  [[1,0,0,0],
-  [1,1,1,0]], //J
-  [[1,1,1,1],
-  [0,0,0,0]], //I
-  [[0,1,1,0],
-  [0,1,1,0]], //O
-  [[0,1,0,0],
-  [1,1,1,0]], //T
-  ]
-*/
 
 data.boxType = [
   [
@@ -73,6 +54,11 @@ data.boxType = [
   ], //Z
   [
     [
+      [0,0,1],
+      [1,1,1],
+      [0,0,0]
+    ],
+    [
       [0,1,0],
       [0,1,0],
       [0,1,1]
@@ -86,11 +72,6 @@ data.boxType = [
       [1,1,0],
       [0,1,0],
       [0,1,0]
-    ],
-    [
-      [0,0,1],
-      [1,1,1],
-      [0,0,0]
     ]
   ], //L
   [
@@ -158,9 +139,13 @@ data.boxType = [
     ] //T
   ]
 ];
+data.boxShift = [
+  [3, 0, 1], [3, 0, 1], [3, 0, 0], [3, 0, 0], [4, 0, 1], [2, 1, 0], [3, 0, 1]
+];//length, shiftX, shiftY
+
+addEvent(starButton, 'click', starGame);
 
 /*
-addEvent(starButton, 'click', starGame);
 addEvent(document, 'keydown', keydownEvent);
 
 function keydownEvent (e) {
@@ -177,7 +162,119 @@ function keydownEvent (e) {
   }
   changeSpanColor();
 }
+*/
 
+function starGame () {
+  initialization();
+  inGame();
+  TIME = setInterval(inGame, data.timeInterval);
+}
+
+function inGame () {
+  if (data.dropBox === false) {
+    createDropBox();
+  } else {
+    //boxDrop();
+  }
+  changeSpanColor();
+}
+
+function createDropBox () {
+  data.dropBox = true;
+  if (data.nextBoxType !== false) {
+    data.currentBoxType = data.nextBoxType;
+  } else {
+    data.currentBoxType = parseInt(Math.random() * data.boxType.length * 100) % data.boxType.length;
+  }
+  data.nextBoxType = parseInt(Math.random() * data.boxType.length * 100) % data.boxType.length;
+  //data.currentBoxType = 6;
+  data.currentBoxState = data.boxShift[data.currentBoxType][0];
+  var length = data.boxShift[data.currentBoxType][0],
+      shiftX = data.boxShift[data.currentBoxType][1],
+      shiftY = data.boxShift[data.currentBoxType][2];
+  outer:for (var i = 0 + shiftY, len = length; i < len; i++) {
+    for (var j = 3 + shiftX; j < 3 + len + shiftX; j++) {
+      if (data.boxType[data.currentBoxType][0][i][j - shiftX - 3] === 1) {   
+        if (data.box[i - shiftY][j].type !== false) {
+          console.log('fail');
+          break outer;
+        }
+        data.box[i - shiftY][j].type = data.currentBoxType + 1;
+      }
+    }
+  }
+}
+
+/*
+function boxDrop () {
+  var flag = true;
+  var limit = [];
+  outer:for (var j = data.col - 1; j >= 0; j--) {
+    for (var i = data.row - 2; i >= 0; i--) {
+      if (data.box[i][j].type >= 1 && data.box[i][j].type <= 7) {
+        limit.push(j);
+        if (data.box[i + 1][j].type !== false) {
+          flag = false;
+          break outer;
+        } else {
+          break;
+        }
+      }
+    }
+  }
+  limit.sort(function (a, b) {
+    return a - b;
+  });
+  var left = limit[0];
+  var right = limit[limit.length - 1];
+  var bottom = false;
+  if (flag) {
+    for (var j = left; j <= right; j++) {
+      for (var i = data.row - 2; i >= 0; i--) {
+        if (data.box[i][j].type >= 1 && data.box[i][j].type <= 7) {
+          data.box[i + 1][j].type = data.box[i][j].type;
+          data.box[i][j].type = false;
+        }
+      }
+    }
+    outer:for (var j = data.col - 1; j >= 0; j--) {
+      for (var i = data.row - 1; i >= 0; i--) {
+        if (data.box[i][j].type >= 1 && data.box[i][j].type <= 7) {
+          if (i === data.row - 1 || data.box[i + 1][j].type == 8) {
+            bottom = true;
+            break outer;
+          } else {
+            break;
+          }
+        }
+      }
+    }
+    if (bottom) {
+      for (var i = data.row - 1; i >= 0; i--) {
+        for (var j = left; j <= right; j++) {
+          if (data.box[i][j].type >= 1 && data.box[i][j].type <= 7) {
+            data.box[i][j].type = 8;
+          } 
+        }
+      }
+      data.dropBox = false;
+    }
+  }
+}
+*/
+function tempShow () {
+  var a = [];
+  for (var i = 0; i < data.row; i++) {
+    for (var j = 0; j < data.col; j++) {
+      a.push(data.box[i][j].type);
+    }
+    console.log(a);
+    a = [];
+  }
+  console.log('_________________');
+}
+
+/*
 function changeDirectionLeft () {
   var flag = true;
   var limit = [];
@@ -347,132 +444,19 @@ function boxLimit () {
   var length = Math.max((bottom - top), (right - left)) + 1;
   return {top, left, length};
 }
-
-function createFrame () {
-  createFrameContext(mainBox, data.row, data.col);
-  createFrameContext(nextBox, 2, 4);
-}
-
-function starGame () {
-  initialization();
-  inGame();
-  TIME = setInterval(inGame, data.timeInterval);
-}
-
-function inGame () {
-  if (data.dropBox === false) {
-    createDropBox();
-  } else {
-    boxDrop();
-  }
-  changeSpanColor();
-}
-
-function createDropBox () {
-  data.dropBox = true;
-  if (data.nextBoxType !== false) {
-    data.currentBoxType = data.nextBoxType;
-  } else {
-    data.currentBoxType = parseInt(Math.random() * 7 * 100) % 7;
-  }
-  data.nextBoxType = parseInt(Math.random() * 7 * 100) % 7;
-  data.currentBoxType = 0;
-  switch (data.currentBoxType) {
-    case 0: data.currentBoxState = 0; break;
-    case 1: data.currentBoxState = 0; break;
-    case 2: data.currentBoxState = 0; break;
-    case 3: data.currentBoxState = 0; break;
-    case 4: data.currentBoxState = 0; break;
-    case 5: data.currentBoxState = 0; break;
-    case 6: data.currentBoxState = 0; break;
-  }
-  for (var i = 0; i < 2; i++) {
-    for (var j = 3; j < 7; j++) {
-      if (data.boxType[data.currentBoxType][i][j - 3] === 1) {
-        data.box[i][j].type = data.currentBoxType + 1;
-      }
-    }
-  }
-}
-
-function boxDrop () {
-  var flag = true;
-  var limit = [];
-  outer:for (var j = data.col - 1; j >= 0; j--) {
-    for (var i = data.row - 2; i >= 0; i--) {
-      if (data.box[i][j].type >= 1 && data.box[i][j].type <= 7) {
-        limit.push(j);
-        if (data.box[i + 1][j].type !== false) {
-          flag = false;
-          break outer;
-        } else {
-          break;
-        }
-      }
-    }
-  }
-  limit.sort(function (a, b) {
-    return a - b;
-  });
-  var left = limit[0];
-  var right = limit[limit.length - 1];
-  var bottom = false;
-  if (flag) {
-    for (var j = left; j <= right; j++) {
-      for (var i = data.row - 2; i >= 0; i--) {
-        if (data.box[i][j].type >= 1 && data.box[i][j].type <= 7) {
-          data.box[i + 1][j].type = data.box[i][j].type;
-          data.box[i][j].type = false;
-        }
-      }
-    }
-    outer:for (var j = data.col - 1; j >= 0; j--) {
-      for (var i = data.row - 1; i >= 0; i--) {
-        if (data.box[i][j].type >= 1 && data.box[i][j].type <= 7) {
-          if (i === data.row - 1 || data.box[i + 1][j].type == 8) {
-            bottom = true;
-            break outer;
-          } else {
-            break;
-          }
-        }
-      }
-    }
-    if (bottom) {
-      for (var i = data.row - 1; i >= 0; i--) {
-        for (var j = left; j <= right; j++) {
-          if (data.box[i][j].type >= 1 && data.box[i][j].type <= 7) {
-            data.box[i][j].type = 8;
-          } 
-        }
-      }
-      data.dropBox = false;
-    }
-  }
-}
-
-function tempShow () {
-  var a = [];
-  for (var i = 0; i < data.row; i++) {
-    for (var j = 0; j < data.col; j++) {
-      a.push(data.box[i][j].type);
-    }
-    console.log(a);
-    a = [];
-  }
-  console.log('_________________');
-}
+*/
 
 function changeSpanColor () {
   for (var i = 0, len = mainBox.getElementsByTagName('span').length; i < len ; i++) {
     var currentBox = data.box[parseInt(i / data.col)][parseInt(i % data.col)];
-    if (currentBox.type >= 1 && currentBox.type <= 7) {
-      currentBox.ele.addClass('typed');
-      currentBox.ele.addClass('type' + currentBox.type);
-    } else if (currentBox.type === 8) {
+    if (currentBox.type === 0) {
       currentBox.ele.className = '';
       currentBox.ele.addClass('typed');
       currentBox.ele.addClass('level' + data.level);
+    } else if (currentBox.type >= 1 && currentBox.type <= 7) {
+      currentBox.ele.className = '';
+      currentBox.ele.addClass('typed');
+      currentBox.ele.addClass('type' + currentBox.type);
     } else {
       currentBox.ele.className = '';
     }
@@ -494,9 +478,15 @@ function initialization () {
       data.box[i][j] = {};
       data.box[i][j].ele = currentSpans[j];
       data.box[i][j].type = false;
-      // 格子类型 false:空 1:S 2:Z 3:L 4:J 5:I 6:O 7:T 8:buttom
+      // 格子类型 false:空 0:buttom 1:S 2:Z 3:L 4:J 5:I 6:O 7:T
     }
   }
+}
+
+
+function createFrame () {
+  createFrameContext(mainBox, data.row, data.col);
+  createFrameContext(nextBox, 2, 4);
 }
 
 function createFrameContext (ele, rowValue, colValue) {
@@ -515,4 +505,3 @@ function createFrameContext (ele, rowValue, colValue) {
     ele.appendChild(row[i]);
   }
 }
-*/
