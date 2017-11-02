dc0818426923b3bbae4b03ebff9ad4ead47c502b
fifthwolf <fifthwolf@qq.com>
2017-11-02 20:35:17 +0800

    update

--- js/gobang.js
+++ js/gobang.js
@@ -8,7 +8,7 @@ window.onload = function () {
 }
 
 var data = {
-  chess = [];
+  chess: []
 }
 
 addEvent(startButton, 'click', startGame);
@@ -17,12 +17,6 @@ function startGame () {
   createFrame();
   resetData();
   addEvent(startButton, 'click', playing);
-}
-
-function playing () { //游戏开始后在棋盘落子
-  //...
-  //amai();
-  victory();
 }
 
 function resetData () { //data.chess数据重置
@@ -34,7 +28,65 @@ function victory () { //判断胜利
 }
 
 function createFrame () { //创建canvas chessBoard棋盘
+  chessBoard.width = 560;
+  chessBoard.height = 560;
+  var context = chessBoard.getContext('2d');
+  _drawBoard(context, 560, 560);
+  _shadowReset(context);
+  _drawLine(context);
+  _drawPoint(context);
 
+  function _drawBoard (cxt, width, height) {
+    cxt.beginPath();
+    var bgColor = cxt.createLinearGradient(0, 0, width, height);
+    bgColor.addColorStop(0, '#ffbf5f');
+    bgColor.addColorStop(1, '#9b5f2b');
+    cxt.fillStyle = bgColor;
+    cxt.shadowColor = '#000';
+    cxt.shadowOffsetX = 5;
+    cxt.shadowOffsetY = 5;
+    cxt.shadowBlur = 5;
+    cxt.fillRect(35-20,35-20,35*14+20*2,35*14+20*2);
+  }
+
+  function _drawLine (cxt) {
+    cxt.fillStyle = "#000";
+    cxt.lineCap = 'square';
+    cxt.lineWidth = 2;
+    for (var i = 1; i <= 15; i++) {
+      cxt.beginPath();
+      cxt.moveTo(35, i * 35);
+      cxt.lineTo(35 * 15, i * 35);
+      cxt.stroke();
+    }
+    for (var i = 1; i <= 15; i++) {
+      cxt.beginPath();
+      cxt.moveTo(i * 35, 35);
+      cxt.lineTo(i * 35, 35 * 15);
+      cxt.stroke();
+    }
+  }
+
+  function _drawPoint (cxt) {
+    var pointPosition = [[4, 4], [4, 12], [8, 8], [12, 4], [12, 12]];
+    for (var i = 0, len = pointPosition.length; i < len; i++) {
+      cxt.beginPath();
+      cxt.arc(pointPosition[i][0] * 35, pointPosition[i][1] * 35, 5, 0, 2 * Math.PI);
+      cxt.fill();
+    }
+  }
+
+  function _shadowReset (cxt) {
+    cxt.shadowOffsetX = 0;
+    cxt.shadowOffsetY = 0;
+    cxt.shadowBlur = 0;
+  }
+}
+
+function playing () { //游戏开始后在棋盘落子
+  //...
+  //amai();
+  victory();
 }
 
 function amai () { //AI落子
