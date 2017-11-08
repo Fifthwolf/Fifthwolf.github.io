2fcc8acd2d0f70a2d602b8e13f360b1436b43638
fifthwolf <fifthwolf@qq.com>
2017-11-08 14:43:25 +0800

    update

--- js/gobangAI.js
+++ js/gobangAI.js
@@ -21,7 +21,7 @@ function amai (chess, currentPlayer) { //AI落子
       if (chess[i][j] !== false) {
         continue;
       }
-      _AIScore(i, j, currentPlayer, AIchess.we);
+      //_AIScore(i, j, currentPlayer, AIchess.we);
       _AIScore(i, j, (currentPlayer + 1) % 2, AIchess.other);
     }
   }
@@ -118,257 +118,185 @@ function amai (chess, currentPlayer) { //AI落子
   function _AIScore (i, j, type, AIchess) { //判断得分
     chess[i][j] = type;
 
+    var chessType = {
+      die4: 0,
+      alive4: 0,
+      die3: 0,
+      alive3: 0,
+      die2: 0,
+      alive2: 0
+    }
+   
     //横向
-    switch (judgeTransverseContinuity(type, i, j)) {
+    switch (judgeTransverseContinuity(type, i, j).position) {
       case 5: AIchess[i][j] += 100000; break;
-      //case 4: AIchess[i][j] += judgeTransverseContinuity4(type, i, j); break; //连4
+      case 4: judgeTransverseContinuity4(type, i, judgeTransverseContinuity(type, i, j).startX); break;
       case 3: AIchess[i][j] += 1000; break;
       case 2: AIchess[i][j] += 100; break;
     }
 
     //纵向
-    switch (judgePortraitContinuity(type, i, j)) {
+    switch (judgePortraitContinuity(type, i, j).position) {
       case 5: AIchess[i][j] += 100000; break;
-      //case 4: AIchess[i][j] += judgePortraitContinuity4(type, i, j); break; //连4
+      case 4: judgePortraitContinuity4(type, j, judgePortraitContinuity(type, i, j).startY); break;
       case 3: AIchess[i][j] += 1000; break;
       case 2: AIchess[i][j] += 100; break;
     }
 
     //正斜
-    switch (judgeInclinedContinuity(type, i, j)) {
+    switch (judgeInclinedContinuity(type, i, j).position) {
       case 5: AIchess[i][j] += 100000; break;
-      //case 4: AIchess[i][j] += judgeInclinedContinuity4(type, i, j); break; //连4
+      case 4: judgeInclinedContinuity4(type, judgePortraitContinuity(type, i, j).startX, judgePortraitContinuity(type, i, j).startY); break;
       case 3: AIchess[i][j] += 1000; break;
       case 2: AIchess[i][j] += 100; break;
     }
 
     //反斜
-    switch (judgeAntiInclinedContinuity(type, i, j)) {
+    switch (judgeAntiInclinedContinuity(type, i, j).position) {
       case 5: AIchess[i][j] += 100000; break;
-      //case 4: AIchess[i][j] += judgeAntiInclinedContinuity4(type, i, j); break; //连4
+      case 4: judgeAntiInclinedContinuity4(type, i, j); break;
       case 3: AIchess[i][j] += 1000; break;
       case 2: AIchess[i][j] += 100; break;
     }
 
+    sumChessScore(i, j);
+
     chess[i][j] = false;
-  }
 
-  function judgeTransverseContinuity (type, row, col) { //判断横向连续
-    var length = 4,
-        limitLeft = Math.max(0, col - length),
-        limitRight = Math.min(14, col + length);
-    var continuity = 0, flag = true;
-    for (var j = limitLeft; j <= limitRight; j++) {
-      if (j === col) {
-        flag = false;
-      }
-      if (chess[row][j] === type) {
-        continuity++;
-      } else {
-        if (flag) {
-          continuity = 0;
+    function judgeTransverseContinuity (type, row, col) { //判断横向连续
+      var length = 4, start;
+      limitLeft = Math.max(0, col - length),
+      limitRight = Math.min(14, col + length);
+      var continuity = 0, flag = true;
+      for (var j = limitLeft; j <= limitRight; j++) {
+        if (j === col) {
+          flag = false;
+        }
+        if (chess[row][j] === type) {
+          continuity++;
         } else {
-          break;
+          if (flag) {
+            continuity = 0;
+          } else {
+            break;
+          }
         }
+        startX = j + 1 - continuity;
       }
+      return {'position': Math.min(5, continuity), 'startX': startX};
     }
-    return Math.min(5, continuity);
-  }
 
-  function judgePortraitContinuity (type, row, col) { //判断纵向连续
-    var length = 4,
-        limitTop = Math.max(0, row - length),
-        limitBottom = Math.min(14, row + length);
-    var continuity = 0, flag = true;
-    for (var i = limitTop; i <= limitBottom; i++) {
-      if (i === row) {
-        flag = false;
-      }
-      if (chess[i][col] === type) {
-        continuity++;
-      } else {
-        if (flag) {
-          continuity = 0;
+    function judgePortraitContinuity (type, row, col) { //判断纵向连续
+      var length = 4,
+      limitTop = Math.max(0, row - length),
+      limitBottom = Math.min(14, row + length);
+      var continuity = 0, flag = true;
+      for (var i = limitTop; i <= limitBottom; i++) {
+        if (i === row) {
+          flag = false;
+        }
+        if (chess[i][col] === type) {
+          continuity++;
         } else {
-          break;
+          if (flag) {
+            continuity = 0;
+          } else {
+            break;
+          }
         }
+        startY = i + 1 - continuity;
       }
+      return {'position': Math.min(5, continuity), 'startY': startY};
     }
-    return Math.min(5, continuity);
-  }
 
-  function judgeInclinedContinuity (type, row, col) { //判断正斜连续
-    var continuity = 0, flag = true;
-    for (var i = row - 4, j = col + 4, len = 0; len < 9; i++, j--, len++) {
-      if (i < 0 || j < 0 || i > 14 || j > 14) {
-        continue;
-      }
-      if (i === row && j === col) {
-        flag = false;
-      }
-      if (chess[i][j] === type) {
-        continuity++;
-      } else {
-        if (flag) {
-          continuity = 0;
+    function judgeInclinedContinuity (type, row, col) { //判断正斜连续
+      var continuity = 0, flag = true;
+      for (var i = row - 4, j = col + 4, len = 0; len < 9; i++, j--, len++) {
+        if (i < 0 || j < 0 || i > 14 || j > 14) {
+          continue;
+        }
+        if (i === row && j === col) {
+          flag = false;
+        }
+        if (chess[i][j] === type) {
+          continuity++;
         } else {
-          break;
+          if (flag) {
+            continuity = 0;
+          } else {
+            break;
+          }
         }
+        startX = j + 1 - continuity;
+        startY = i + 1 - continuity;
       }
+      return {'position': Math.min(5, continuity), 'startX': startX, 'startY': startY};
     }
-    return Math.min(5, continuity);
-  }
 
-  function judgeAntiInclinedContinuity (type, row, col) { //判断反斜连续
-    var continuity = 0, flag = true;
-    for (var i = row - 4, j = col - 4, len = 0; len < 9; i++, j++, len++) {
-      if (i < 0 || j < 0 || i > 14 || j > 14) {
-        continue;
-      }
-      if (i === row && j === col) {
-        flag = false;
-      }
-      if (chess[i][j] === type) {
-        continuity++;
-      } else {
-        if (flag) {
-          continuity = 0;
+    function judgeAntiInclinedContinuity (type, row, col) { //判断反斜连续
+      var continuity = 0, flag = true;
+      for (var i = row - 4, j = col - 4, len = 0; len < 9; i++, j++, len++) {
+        if (i < 0 || j < 0 || i > 14 || j > 14) {
+          continue;
+        }
+        if (i === row && j === col) {
+          flag = false;
+        }
+        if (chess[i][j] === type) {
+          continuity++;
         } else {
-          break;
+          if (flag) {
+            continuity = 0;
+          } else {
+            break;
+          }
         }
+        startX = j + 1 - continuity;
+        startY = i + 1 - continuity;
+      }
+      return {'position': Math.min(5, continuity), 'startX': startX, 'startY': startY};
+    }
+
+    function judgeTransverseContinuity4 (type, row, startX) { //横向连4判断
+      if (startX - 1 >= 0 && startX + 4 <= 14) {
+        if (chess[row][startX - 1] === false && chess[row][startX + 4] === false) {
+          chessType.alive4++;
+        } else if (chess[row][startX - 1] !== false && chess[row][startX + 4] !== false) {
+          //nothing
+        } else {
+          chessType.die4++;
+        }
+      } else if (startX - 1 < 0 && chess[row][startX + 4] === false) {
+        chessType.die4++;
+      } else if (startX + 4 > 14 && chess[row][startX - 1] === false) {
+        chessType.die4++;
       }
     }
-    return Math.min(5, continuity);
+
+    function judgePortraitContinuity4 (type, col, startY) { //纵向连4判断
+      if (startY - 1 >= 0 && startY + 4 <= 14) {
+        if (chess[startY - 1][col] === false && chess[startY + 4][col] === false) {
+          chessType.alive4++;
+        } else if (chess[startY - 1][col] !== false && chess[startY + 4][col] !== false) {
+          //nothing
+        } else {
+          chessType.die4++;
+        }
+      } else if (startY - 1 < 0 && chess[startY + 4][col] === false) {
+        chessType.die4++;
+      } else if (startY + 4 > 14 && chess[startY - 1][col] === false) {
+        chessType.die4++;
+      }
+    }
+
+    function judgeInclinedContinuity4 (type ,startX, startY) {
+
+    }
+
+    function sumChessScore (i, j) {
+      if (chessType.alive4 >= 1 || chessType.die4 >= 2 || chessType.die4 >= 1 && chessType.alive3 >= 1) {
+        AIchess[i][j] += 10000;
+      }
+    }
   }
-
-  /*
-  function judgeContinuity4 (type, row, col) { //判断连4
-    var length = 4, continuityChess = 4, death4 = 0;
-    var limitLeft = Math.max(0, col - length),
-        limitRight = Math.min(14, col + length);
-        limitTop = Math.max(0, row - length),
-        limitBottom = Math.min(14, row + length);
-
-    // 横向判断
-    var continuity = 0;
-    for (var j = limitLeft; j <= limitRight; j++) {
-      chess[row][j] === type ? continuity++ : continuity = 0;
-      if (continuity == continuityChess) {
-        if (j + 1 <= 14  && j - 4 >= 0) {
-          if (chess[row][j + 1] === false && chess[row][j - 4] === false) {
-            return 10000; //活4
-          }
-        } else if (j + 1 <= 14 && chess[row][j + 1] === false || j - 4 >= 0 && chess[row][j - 4] === false) {
-          death4++; //死4
-        }
-      }
-    }
-
-    //纵向判断
-    continuity = 0;
-    for (var i = limitTop; i <= limitBottom; i++) {
-      chess[i][col] === type ? continuity++ : continuity = 0;
-      if (continuity == continuityChess) {
-        if (row + 1 <= 14 && row - 4 >= 0) {
-          if (chess[i + 1][col] === false && chess[i - 4][col] === false) {
-            return 10000; //活4
-          }
-        } else if (row + 1 <= 14 && chess[row + 1][j] === false || row - 4 >= 0 && chess[row - 4][j] === false) {
-          death4++; //死4
-        }
-      }
-    }
-
-    //正斜判断
-    continuity = 0;
-    for (var i = row - 4, j = col + 4, len = 0; len < 9; i++, j--, len++) {
-      if (i < 0 || j < 0 || i > 14 || j > 14) {
-        continue;
-      }
-      chess[i][j] === type ? continuity++ : continuity = 0;
-      if (continuity == continuityChess) {
-        if (row + 1 <= 14 && j - 1 >= 0 && row - 4 >= 0 && j + 4 <= 14) {
-          if (chess[row + 1][j - 1] === false && chess[row - 4][j + 4] === false) {
-            return 10000; //活4
-          }
-        } else if (row + 1 <= 14 && j - 1 >= 0 && chess[row + 1][j - 1] === false ||
-          row - 4 >= 0 && j + 4 <= 14 && chess[row - 4][j + 4] === false) {
-          death4++; //死4
-        }
-      }
-    }
-
-    //反斜判断
-    continuity = 0;
-    for (var i = row - 4, j = col - 4, len = 0; len < 9; i++, j++, len++) {
-      if (i < 0 || j < 0 || i > 14 || j > 14) {
-        continue;
-      }
-      chess[i][j] === type ? continuity++ : continuity = 0;
-      if (continuity == continuityChess) {
-        if (row + 1 <= 14 && j + 1 <= 14 && row - 4 >= 0 && j - 4 >= 0) {
-          if (chess[row + 1][j + 1] === false && chess[row - 4][j - 4] === false) {
-            return 10000; //活4
-          }
-        } else if (row + 1 <= 14 && j + 1 <= 14 && chess[row + 1][j + 1] === false ||
-          row - 4 >= 0 && j - 4 >= 0 && chess[row - 4][j - 4] === false) {
-          death4++; //死4
-        }
-      }
-    }
-    if (death4 >= 2) {
-      return 10000;
-    }
-  
-    return false;
-  }
-  */
 }
-  
-
-/*
-data.chess[4][6] = 0;
-data.chess[4][8] = 0;
-data.chess[5][8] = 0;
-data.chess[5][10] = 0;
-data.chess[6][7] = 0;
-data.chess[6][9] = 0;
-data.chess[7][6] = 0;
-data.chess[7][7] = 0;
-data.chess[7][8] = 0;
-data.chess[7][10] = 0;
-data.chess[8][11] = 0;
-drawChess(6,4,0);
-drawChess(8,4,0);
-drawChess(8,5,0);
-drawChess(10,5,0);
-drawChess(7,6,0);
-drawChess(9,6,0);
-drawChess(6,7,0);
-drawChess(7,7,0);
-drawChess(8,7,0);
-drawChess(10,7,0);
-drawChess(11,8,0);
-data.chess[3][5] = 1;
-data.chess[3][9] = 1;
-data.chess[4][7] = 1;
-data.chess[4][9] = 1;
-data.chess[5][7] = 1;
-data.chess[6][6] = 1;
-data.chess[6][8] = 1;
-data.chess[7][5] = 1;
-data.chess[7][9] = 1;
-data.chess[8][7] = 1;
-data.chess[9][12] = 1;
-drawChess(5,3,1);
-drawChess(9,3,1);
-drawChess(7,4,1);
-drawChess(9,4,1);
-drawChess(7,5,1);
-drawChess(6,6,1);
-drawChess(8,6,1);
-drawChess(5,7,1);
-drawChess(9,7,1);
-drawChess(7,8,1);
-drawChess(12,9,1);
-*/
