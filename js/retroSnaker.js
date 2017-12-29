ebe53642cd924d9f1a327bb24c3c4320521b8603
fifthwolf <fifthwolf@qq.com>
2017-12-29 12:43:23 +0800

    format js

--- js/retroSnaker.js
+++ js/retroSnaker.js
@@ -1,16 +1,16 @@
-﻿var content = document.getElementsByClassName('content')[0];
-var scoreBox = content.getElementsByClassName('score')[0];
-var pauseButton = content.getElementsByClassName('pause')[0];
-var timeBox = content.getElementsByClassName('time')[0];
-var mainViewBox = content.getElementsByClassName('box')[0];
-var cellMask = content.getElementsByClassName('cellMask')[0];
-var starButton = cellMask.getElementsByClassName('starButton')[0];
-var final = cellMask.getElementsByClassName('final')[0];
+﻿var content = document.getElementsByClassName('content')[0],
+  scoreBox = content.getElementsByClassName('score')[0],
+  pauseButton = content.getElementsByClassName('pause')[0],
+  timeBox = content.getElementsByClassName('time')[0],
+  mainViewBox = content.getElementsByClassName('box')[0],
+  cellMask = content.getElementsByClassName('cellMask')[0],
+  starButton = cellMask.getElementsByClassName('starButton')[0],
+  final = cellMask.getElementsByClassName('final')[0];
 var TIME;
 var GAMERUN;
 
-window.onload = function () {
-  delayedLoadingPublicPictures ('../');
+window.onload = function() {
+  delayedLoadingPublicPictures('../');
   createFrame();
   if (!judgeWidth()) {
     mobileControl();
@@ -35,14 +35,14 @@ addEvent(starButton, 'click', starGame);
 addEvent(document, 'keydown', keydownChange);
 addEvent(pauseButton, 'click', pauseGame);
 
-function mobileControl () {
+function mobileControl() {
   var mobileControl = document.getElementById('mobileControl');
   var controlCanvas = document.getElementById('controlCanvas');
   var mobileButton = document.getElementById('mobileButton');
   drawControl();
   mobileControlTouch(mobileControl);
 
-  function drawControl () {
+  function drawControl() {
     controlCanvas.width = 300;
     controlCanvas.height = 300;
     var context = controlCanvas.getContext('2d');
@@ -56,18 +56,18 @@ function mobileControl () {
     _drawArc(context, 300, 0);
     _drawArc(context, 300, 300);
 
-    function _drawArc (ctx, x, y) {
+    function _drawArc(ctx, x, y) {
       ctx.beginPath();
       ctx.arc(x, y, 120, 0, 2 * Math.PI);
       ctx.stroke();
     }
   }
 
-  function mobileControlTouch (element) {
+  function mobileControlTouch(element) {
     var buttonX, buttonY;
     var elementPosition = element.getBoundingClientRect();
 
-    function buttonMove (e, flag) {
+    function buttonMove(e, flag) {
       if (flag) {
         e.preventDefault();
         var touch = e.changedTouches;
@@ -87,16 +87,16 @@ function mobileControl () {
           buttonY = Math.sin(angle) * 150;
         } else {
           buttonX = 0;
-        }  
+        }
       }
       if (flag) {
         _cons(buttonX, buttonY);
       }
-      mobileButton.style.left = buttonX + 98 +'px';
-      mobileButton.style.top = buttonY + 98 +'px';   
+      mobileButton.style.left = buttonX + 98 + 'px';
+      mobileButton.style.top = buttonY + 98 + 'px';
     }
 
-    function _cons (buttonX, buttonY) {
+    function _cons(buttonX, buttonY) {
       if (buttonX * buttonX + buttonY * buttonY > 50 * 50) {
         if (Math.abs(buttonX) > Math.abs(buttonY)) {
           if (buttonX > 0) {
@@ -114,30 +114,30 @@ function mobileControl () {
       }
     }
 
-    addEvent(element, 'touchstart', function (e) {
+    addEvent(element, 'touchstart', function(e) {
       buttonMove(e, true);
     });
 
-    addEvent(element, 'touchmove', function (e) {
+    addEvent(element, 'touchmove', function(e) {
       buttonMove(e, true);
     });
 
-    addEvent(element, 'touchend', function (e) {
+    addEvent(element, 'touchend', function(e) {
       var touch = e.changedTouches;
       buttonMove(e, false);
     });
   }
 }
 
-function createFrame () {
+function createFrame() {
   mainViewBox.innerHTML = '';
   data.box = new Array(data.row);
   for (var i = 0; i < data.row; i++) {
     data.box[i] = new Array(data.col);
     var row = new Array(data.row);
     row[i] = document.createElement('div');
-    row[i].setAttribute('row',i);
-    for (var j = 0; j < data.col; j++) { 
+    row[i].setAttribute('row', i);
+    for (var j = 0; j < data.col; j++) {
       var col = new Array(data.col);
       col[j] = document.createElement('span');
       col[j].setAttribute('col', j);
@@ -148,38 +148,40 @@ function createFrame () {
   }
 }
 
-function starGame () {
+function starGame() {
   dataReset();
   data.pretreatmentDirection = parseInt(Math.random() * 100 % 4 + 1);
   data.direction = data.pretreatmentDirection;
   switch (data.direction) {
     case 1:
-    data.snake.push([data.snake[0][0] + 1, data.snake[0][1]]);
-    data.snake.push([data.snake[0][0] + 2, data.snake[0][1]]);
-    break;
+      data.snake.push([data.snake[0][0] + 1, data.snake[0][1]]);
+      data.snake.push([data.snake[0][0] + 2, data.snake[0][1]]);
+      break;
     case 2:
-    data.snake.push([data.snake[0][0], data.snake[0][1] - 1]);
-    data.snake.push([data.snake[0][0], data.snake[0][1] - 2]);
-    break;
+      data.snake.push([data.snake[0][0], data.snake[0][1] - 1]);
+      data.snake.push([data.snake[0][0], data.snake[0][1] - 2]);
+      break;
     case 3:
-    data.snake.push([data.snake[0][0] - 1, data.snake[0][1]]);
-    data.snake.push([data.snake[0][0] - 2, data.snake[0][1]]);
-    break;
+      data.snake.push([data.snake[0][0] - 1, data.snake[0][1]]);
+      data.snake.push([data.snake[0][0] - 2, data.snake[0][1]]);
+      break;
     case 4:
-    data.snake.push([data.snake[0][0], data.snake[0][1] + 1]);
-    data.snake.push([data.snake[0][0], data.snake[0][1] + 2]);
-    break;
+      data.snake.push([data.snake[0][0], data.snake[0][1] + 1]);
+      data.snake.push([data.snake[0][0], data.snake[0][1] + 2]);
+      break;
   }
   createEatPoing();
   draw();
   GAMERUN = setInterval(running, 200);
   TIME = setInterval(timeAdd, 100);
 
-  function dataReset () {
+  function dataReset() {
     cellMask.style.display = 'none';
     pauseButton.style.display = 'inline-block';
     data.start = true;
-    data.snake = [[10, 15]];
+    data.snake = [
+      [10, 15]
+    ];
     data.time = 0;
     data.score = 0;
     timeBox.innerHTML = PrefixInteger(data.time, 3);
@@ -187,7 +189,7 @@ function starGame () {
   }
 }
 
-function pauseGame () {
+function pauseGame() {
   if (data.pause === false) {
     data.pause = true;
     pauseButton.innerHTML = 'CONTINUE';
@@ -201,26 +203,34 @@ function pauseGame () {
   }
 }
 
-function timeAdd () {
+function timeAdd() {
   data.time += 1;
   timeBox.innerHTML = PrefixInteger(parseInt(data.time / 10), 3);
 }
 
-function keydownChange (e) {
+function keydownChange(e) {
   var keynum = window.event ? e.keyCode : e.which;
   switch (keynum) {
     //左
-    case 37: changeDirection(4); break;
-    //上
-    case 38: changeDirection(1); break;
-    //右
-    case 39: changeDirection(2); break;
-    //下
-    case 40: changeDirection(3); break;
+    case 37:
+      changeDirection(4);
+      break;
+      //上
+    case 38:
+      changeDirection(1);
+      break;
+      //右
+    case 39:
+      changeDirection(2);
+      break;
+      //下
+    case 40:
+      changeDirection(3);
+      break;
   }
 }
 
-function changeDirection (direction) {
+function changeDirection(direction) {
   if ((direction === 2 || direction === 4) && (data.direction === 1 || data.direction === 3)) {
     data.pretreatmentDirection = direction;
   }
@@ -229,24 +239,24 @@ function changeDirection (direction) {
   }
 }
 
-function running () {
+function running() {
   if (data.pretreatmentDirection != data.direction) {
     data.direction = data.pretreatmentDirection;
   }
   var head = [data.snake[0][0], data.snake[0][1]];
   switch (data.direction) {
     case 1:
-    data.snake.unshift([data.snake[0][0] - 1, data.snake[0][1]]);
-    break;
+      data.snake.unshift([data.snake[0][0] - 1, data.snake[0][1]]);
+      break;
     case 2:
-    data.snake.unshift([data.snake[0][0], data.snake[0][1] + 1]);
-    break;
+      data.snake.unshift([data.snake[0][0], data.snake[0][1] + 1]);
+      break;
     case 3:
-    data.snake.unshift([data.snake[0][0] + 1, data.snake[0][1]]);
-    break;
+      data.snake.unshift([data.snake[0][0] + 1, data.snake[0][1]]);
+      break;
     case 4:
-    data.snake.unshift([data.snake[0][0], data.snake[0][1] - 1]);
-    break;
+      data.snake.unshift([data.snake[0][0], data.snake[0][1] - 1]);
+      break;
   }
   if (head[0] === data.eattingPoint[0] && head[1] === data.eattingPoint[1]) {
     createEatPoing();
@@ -262,7 +272,7 @@ function running () {
   draw();
 }
 
-function createEatPoing () {
+function createEatPoing() {
   do {
     var flag = false;
     var row = parseInt(Math.random() * 600 % data.row);
@@ -273,11 +283,11 @@ function createEatPoing () {
         break;
       }
     }
-  } while ( flag )
+  } while (flag)
   data.eattingPoint = [row, col];
 }
 
-function judgeFail () {
+function judgeFail() {
   //碰壁
   if (data.snake[0][0] < 0 || data.snake[0][0] > data.row - 1 || data.snake[0][1] < 0 || data.snake[0][1] > data.col - 1) {
     return true;
@@ -290,7 +300,7 @@ function judgeFail () {
   }
 }
 
-function gameFail () {
+function gameFail() {
   clearInterval(GAMERUN);
   clearInterval(TIME);
   cellMask.style.display = 'block';
@@ -301,7 +311,7 @@ function gameFail () {
   pauseButton.style.display = 'none';
 }
 
-function draw () {
+function draw() {
   for (var i = 0; i < data.row; i++) {
     for (var j = 0; j < data.col; j++) {
       data.box[i][j].className = '';
