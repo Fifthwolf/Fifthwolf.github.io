fdb8d45ae3cadc691a7a241e8b012000e4b968ba
fifthwolf <fifthwolf@qq.com>
2018-01-01 11:33:08 +0800

    format js

--- js/tetris.js
+++ js/tetris.js
@@ -1,18 +1,18 @@
-var content = document.getElementsByClassName('content')[0];
-var mainBox = document.getElementById('mainBox');
-var cellMask = document.getElementById('cellMask');
-var maskScore = document.getElementById('maskScore');
-var showView = content.getElementsByClassName('showView')[0];
-var nextBox = showView.getElementsByClassName('nextBox')[0];
-var levelBox = document.getElementById('level');
-var lineBox = document.getElementById('line');
-var scoreBox = document.getElementById('score');
-var starButton = document.getElementById('start');
+var content = document.getElementsByClassName('content')[0],
+  mainBox = document.getElementById('mainBox'),
+  cellMask = document.getElementById('cellMask'),
+  maskScore = document.getElementById('maskScore'),
+  showView = content.getElementsByClassName('showView')[0],
+  nextBox = showView.getElementsByClassName('nextBox')[0],
+  levelBox = document.getElementById('level'),
+  lineBox = document.getElementById('line'),
+  scoreBox = document.getElementById('score'),
+  starButton = document.getElementById('start');
 var TIME;
 var mobileControlTIME = {};
 
-window.onload = function () {
-  delayedLoadingPublicPictures ('../');
+window.onload = function() {
+  delayedLoadingPublicPictures('../');
   createFrame();
   if (!judgeWidth()) {
     mobileControl();
@@ -41,124 +41,130 @@ var data = {
 data.boxType = [
   [
     [
-      [0,0,0],
-      [0,1,1],
-      [1,1,0]
+      [0, 0, 0],
+      [0, 1, 1],
+      [1, 1, 0]
     ],
     [
-      [0,1,0],
-      [0,1,1],
-      [0,0,1]
+      [0, 1, 0],
+      [0, 1, 1],
+      [0, 0, 1]
     ]
   ], //S
   [
     [
-      [0,0,0],
-      [1,1,0],
-      [0,1,1]
+      [0, 0, 0],
+      [1, 1, 0],
+      [0, 1, 1]
     ],
     [
-      [0,0,1],
-      [0,1,1],
-      [0,1,0]
+      [0, 0, 1],
+      [0, 1, 1],
+      [0, 1, 0]
     ]
   ], //Z
   [
     [
-      [0,0,1],
-      [1,1,1],
-      [0,0,0]
+      [0, 0, 1],
+      [1, 1, 1],
+      [0, 0, 0]
     ],
     [
-      [0,1,0],
-      [0,1,0],
-      [0,1,1]
+      [0, 1, 0],
+      [0, 1, 0],
+      [0, 1, 1]
     ],
     [
-      [0,0,0],
-      [1,1,1],
-      [1,0,0]
+      [0, 0, 0],
+      [1, 1, 1],
+      [1, 0, 0]
     ],
     [
-      [1,1,0],
-      [0,1,0],
-      [0,1,0]
+      [1, 1, 0],
+      [0, 1, 0],
+      [0, 1, 0]
     ]
   ], //L
   [
     [
-      [1,0,0],
-      [1,1,1],
-      [0,0,0]
+      [1, 0, 0],
+      [1, 1, 1],
+      [0, 0, 0]
     ],
     [
-      [0,1,1],
-      [0,1,0],
-      [0,1,0]
+      [0, 1, 1],
+      [0, 1, 0],
+      [0, 1, 0]
     ],
     [
-      [0,0,0],
-      [1,1,1],
-      [0,0,1]
+      [0, 0, 0],
+      [1, 1, 1],
+      [0, 0, 1]
     ],
     [
-      [0,1,0],
-      [0,1,0],
-      [1,1,0]
+      [0, 1, 0],
+      [0, 1, 0],
+      [1, 1, 0]
     ]
   ], //J
   [
     [
-      [0,0,0,0],
-      [1,1,1,1],
-      [0,0,0,0],
-      [0,0,0,0],
+      [0, 0, 0, 0],
+      [1, 1, 1, 1],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
     ],
     [
-      [0,1,0,0],
-      [0,1,0,0],
-      [0,1,0,0],
-      [0,1,0,0],
+      [0, 1, 0, 0],
+      [0, 1, 0, 0],
+      [0, 1, 0, 0],
+      [0, 1, 0, 0],
     ]
   ], //I
   [
     [
-      [1,1],
-      [1,1]
+      [1, 1],
+      [1, 1]
     ]
   ], //O
   [
     [
-      [0,0,0],
-      [1,1,1],
-      [0,1,0]
+      [0, 0, 0],
+      [1, 1, 1],
+      [0, 1, 0]
     ],
     [
-      [0,1,0],
-      [1,1,0],
-      [0,1,0]
+      [0, 1, 0],
+      [1, 1, 0],
+      [0, 1, 0]
     ],
     [
-      [0,1,0],
-      [1,1,1],
-      [0,0,0]
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 0, 0]
     ],
     [
-      [0,1,0],
-      [0,1,1],
-      [0,1,0]
+      [0, 1, 0],
+      [0, 1, 1],
+      [0, 1, 0]
     ] //T
   ]
 ];
 
 data.boxShift = [
-  [3, 0, 1, 2], [3, 0, 1, 2], [3, 0, 0, 4], [3, 0, 0, 4], [4, 0, 1, 2], [2, 1, 0, 1], [3, 0, 1, 4]
-];//length, shiftX, shiftY, rotateType
+  [3, 0, 1, 2],
+  [3, 0, 1, 2],
+  [3, 0, 0, 4],
+  [3, 0, 0, 4],
+  [4, 0, 1, 2],
+  [2, 1, 0, 1],
+  [3, 0, 1, 4]
+]; //length, shiftX, shiftY, rotateType
 
 addEvent(starButton, 'click', startGame);
 addEvent(document, 'keydown', keydownEvent);
 
-function startGame () {
+function startGame() {
   if (data.start === false) { //游戏未开始
     if (data.pause === false) {
       cellMask.style.display = 'none';
@@ -181,24 +187,24 @@ function startGame () {
   }
 }
 
-function keydownEvent (e) {
+function keydownEvent(e) {
   var keynum = window.event ? e.keyCode : e.which;
   changeBoxState(e, keynum);
 }
 
-function mobileControl () {
+function mobileControl() {
   var mobileControlDiv = document.getElementById('mobileControl');
-  addEvent(mobileControlDiv, 'touchstart', function (e) {
+  addEvent(mobileControlDiv, 'touchstart', function(e) {
     e.preventDefault();
     var ele = e.target;
     var command = parseInt(ele.getAttribute('func'));
     ele.addClass('hover');
     changeBoxState(e, command);
-    mobileControlTIME[command] = setInterval(function () {
+    mobileControlTIME[command] = setInterval(function() {
       changeBoxState(e, command);
     }, 80);
   });
-  addEvent(mobileControlDiv, 'touchend', function (e) {
+  addEvent(mobileControlDiv, 'touchend', function(e) {
     e.preventDefault();
     var ele = e.target;
     var command = parseInt(ele.getAttribute('func'));
@@ -207,28 +213,40 @@ function mobileControl () {
   });
 }
 
-function changeBoxState (e, keynum) {
+function changeBoxState(e, keynum) {
   if (data.start === true && data.pause === false) {
     switch (keynum) {
       //左
-      case 37: changeDirectionLeft(); e.preventDefault(); break;
-      //上
-      case 38: changeRotate(); e.preventDefault(); break;
-      //右
-      case 39: changeDirectionRight(); e.preventDefault(); break;
-      //下
-      case 40: boxDrop(); e.preventDefault(); break;
+      case 37:
+        changeDirectionLeft();
+        e.preventDefault();
+        break;
+        //上
+      case 38:
+        changeRotate();
+        e.preventDefault();
+        break;
+        //右
+      case 39:
+        changeDirectionRight();
+        e.preventDefault();
+        break;
+        //下
+      case 40:
+        boxDrop();
+        e.preventDefault();
+        break;
     }
     changeSpanColor(mainBox, data.box, data.col);
   }
 }
 
-function inGame () {
+function inGame() {
   boxDrop();
   changeSpanColor(mainBox, data.box, data.col);
 }
 
-function createDropBox () {
+function createDropBox() {
   if (data.nextBoxType !== false) {
     data.currentBoxType = data.nextBoxType;
   } else {
@@ -236,13 +254,13 @@ function createDropBox () {
   }
   data.nextBoxType = parseInt(Math.random() * data.boxType.length * 100) % data.boxType.length;
   var length = data.boxShift[data.currentBoxType][0],
-      shiftX = data.boxShift[data.currentBoxType][1],
-      shiftY = data.boxShift[data.currentBoxType][2];
+    shiftX = data.boxShift[data.currentBoxType][1],
+    shiftY = data.boxShift[data.currentBoxType][2];
   data.currentBoxBase = [0 - shiftY, 3 + shiftX];
   data.currentBoxRotate = 0;
-  outer:for (var i = 0 + shiftY, len = length; i < len; i++) {
+  outer: for (var i = 0 + shiftY, len = length; i < len; i++) {
     for (var j = 3 + shiftX; j < 3 + len + shiftX; j++) {
-      if (data.boxType[data.currentBoxType][0][i][j - shiftX - 3] === 1) {   
+      if (data.boxType[data.currentBoxType][0][i][j - shiftX - 3] === 1) {
         if (data.box[i - shiftY][j].type !== false) {
           fail();
           break outer;
@@ -252,7 +270,7 @@ function createDropBox () {
     }
   }
   var nextShiftX = data.boxShift[data.nextBoxType][1],
-      nextShiftY = data.boxShift[data.nextBoxType][2];
+    nextShiftY = data.boxShift[data.nextBoxType][2];
   for (var i = 0; i < 2; i++) {
     for (var j = 0; j < 4; j++) {
       if (data.boxType[data.nextBoxType][0][i + nextShiftY][j - nextShiftX] === 1) {
@@ -260,17 +278,19 @@ function createDropBox () {
       } else {
         data.nextBox[i][j].type = false;
       }
-    } 
+    }
   }
   changeSpanColor(mainBox, data.box, data.col);
   changeSpanColor(nextBox, data.nextBox, 4);
 }
 
-function boxDrop () {
+function boxDrop() {
   var flag = true;
-  var left = boxLimit().left, right = boxLimit().right,
-      top = boxLimit().top, bottom = boxLimit().bottom;
-  outer:for (var j = left; j <= right; j++) {
+  var left = boxLimit().left,
+    right = boxLimit().right,
+    top = boxLimit().top,
+    bottom = boxLimit().bottom;
+  outer: for (var j = left; j <= right; j++) {
     for (var i = top; i <= bottom; i++) {
       if (data.box[i][j].type >= 1 && data.box[i][j].type <= 7) {
         if (i + 1 === 20 || data.box[i + 1][j].type === 0) {
@@ -287,7 +307,7 @@ function boxDrop () {
       for (var i = bottom; i >= top; i--) {
         if (data.box[i][j].type >= 1 && data.box[i][j].type <= 7) {
           data.box[i + 1][j].type = data.box[i][j].type;
-          data.box[i][j].type = false; 
+          data.box[i][j].type = false;
         }
       }
     }
@@ -303,11 +323,13 @@ function boxDrop () {
   }
 }
 
-function changeDirectionLeft () {
+function changeDirectionLeft() {
   var flag = true;
-  var left = boxLimit().left, right = boxLimit().right,
-      top = boxLimit().top, bottom = boxLimit().bottom;
-  outer:for (var i = top; i <= bottom; i++) {
+  var left = boxLimit().left,
+    right = boxLimit().right,
+    top = boxLimit().top,
+    bottom = boxLimit().bottom;
+  outer: for (var i = top; i <= bottom; i++) {
     for (var j = left; j <= right; j++) {
       if (data.box[i][j].type >= 1 && data.box[i][j].type <= 7) {
         if (j <= 0 || data.box[i][j - 1].type === 0) {
@@ -332,11 +354,13 @@ function changeDirectionLeft () {
   }
 }
 
-function changeDirectionRight () {
+function changeDirectionRight() {
   var flag = true;
-  var left = boxLimit().left, right = boxLimit().right,
-      top = boxLimit().top, bottom = boxLimit().bottom;
-  outer:for (var i = top; i <= bottom; i++) {
+  var left = boxLimit().left,
+    right = boxLimit().right,
+    top = boxLimit().top,
+    bottom = boxLimit().bottom;
+  outer: for (var i = top; i <= bottom; i++) {
     for (var j = right; j >= left; j--) {
       if (data.box[i][j].type >= 1 && data.box[i][j].type <= 7) {
         if (j >= data.col - 1 || data.box[i][j + 1].type === 0) {
@@ -361,13 +385,15 @@ function changeDirectionRight () {
   }
 }
 
-function changeRotate () {
+function changeRotate() {
   var flag = true;
-  var left = boxLimit().left, right = boxLimit().right,
-      top = boxLimit().top, bottom = boxLimit().bottom;
+  var left = boxLimit().left,
+    right = boxLimit().right,
+    top = boxLimit().top,
+    bottom = boxLimit().bottom;
   var baseLimit = data.boxShift[data.currentBoxType][0];
   var tempBoxRotate = (data.currentBoxRotate + 1) % data.boxShift[data.currentBoxType][3];
-  outer:for (var i = 0; i < baseLimit; i++) {
+  outer: for (var i = 0; i < baseLimit; i++) {
     for (var j = 0; j < baseLimit; j++) {
       if (data.boxType[data.currentBoxType][tempBoxRotate][i][j] === 1) {
         try {
@@ -375,11 +401,11 @@ function changeRotate () {
             flag = false;
             break outer;
           }
-        } catch(e) {
+        } catch (e) {
           flag = false;
           //Out of range
         }
-      } 
+      }
     }
   }
   if (flag) {
@@ -401,9 +427,12 @@ function changeRotate () {
   }
 }
 
-function boxLimit () {
-  var limitX = [], limitY = [], flag = true, starBox = false;
-  outer:for (var i = 0; i < data.row; i++) {
+function boxLimit() {
+  var limitX = [],
+    limitY = [],
+    flag = true,
+    starBox = false;
+  outer: for (var i = 0; i < data.row; i++) {
     if (starBox === true) {
       flag = false;
     }
@@ -421,10 +450,10 @@ function boxLimit () {
       break outer;
     }
   }
-  limitX.sort(function (a, b) {
+  limitX.sort(function(a, b) {
     return a - b;
   });
-  limitY.sort(function (a, b) {
+  limitY.sort(function(a, b) {
     return a - b;
   });
   var top = limitY[0];
@@ -432,11 +461,17 @@ function boxLimit () {
   var left = limitX[0];
   var right = limitX[limitX.length - 1];
   var length = Math.max((bottom - top), (right - left)) + 1;
-  return {top, bottom, left, right, length};
+  return {
+    top,
+    bottom,
+    left,
+    right,
+    length
+  };
 }
 
-function changeSpanColor (ele, transferData, col) {
-  for (var i = 0, len = ele.getElementsByTagName('span').length; i < len ; i++) {
+function changeSpanColor(ele, transferData, col) {
+  for (var i = 0, len = ele.getElementsByTagName('span').length; i < len; i++) {
     var currentBox = transferData[parseInt(i / col)][parseInt(i % col)];
     if (currentBox.type === 0) {
       currentBox.ele.className = '';
@@ -452,13 +487,13 @@ function changeSpanColor (ele, transferData, col) {
   }
 }
 
-function judgeLineFull () {
+function judgeLineFull() {
   var flag, line = [];
-  outer:for (var i = data.row - 1; i >= 0; i--) {
+  outer: for (var i = data.row - 1; i >= 0; i--) {
     flag = true;
     for (var j = 0; j < data.col; j++) {
       if (data.box[i][j].type !== 0) {
-        flag = false;  
+        flag = false;
         break;
       }
     }
@@ -479,10 +514,12 @@ function judgeLineFull () {
     createDropBox();
   }
 
-  function _animation (line) {
-    var opacity = 1, alphaOpacity = 0;
+  function _animation(line) {
+    var opacity = 1,
+      alphaOpacity = 0;
     opacityAnimation();
-    function opacityAnimation () {
+
+    function opacityAnimation() {
       opacity -= 0.05;
       alphaOpacity += 5;
       for (var i = 0; i < data.col; i++) {
@@ -505,14 +542,14 @@ function judgeLineFull () {
     }
   }
 
-  function _refreshAllBox () {
+  function _refreshAllBox() {
     flag = true;
     for (var times = 0; times < clearTimes; times++) {
       for (var j = 0; j < data.col; j++) {
         for (var i = line[0] + times, clearLine = line[0]; i >= 1; i--) {
           data.box[i][j].type = data.box[i - 1][j].type;
-        } 
-      }  
+        }
+      }
       line.shift();
     }
     createDropBox();
@@ -520,21 +557,35 @@ function judgeLineFull () {
     TIME = setInterval(inGame, data.timeInterval);
   }
 
-  function _upLevel () {
+  function _upLevel() {
     data.level = Math.min(parseInt(data.line / 30) + 1, 7);
     switch (data.level) {
-      case 1: data.timeInterval = 1000; break;
-      case 2: data.timeInterval = 850; break;
-      case 3: data.timeInterval = 700; break;
-      case 4: data.timeInterval = 550; break;
-      case 5: data.timeInterval = 400; break;
-      case 6: data.timeInterval = 200; break;
-      case 7: data.timeInterval = 100; break;
+      case 1:
+        data.timeInterval = 1000;
+        break;
+      case 2:
+        data.timeInterval = 850;
+        break;
+      case 3:
+        data.timeInterval = 700;
+        break;
+      case 4:
+        data.timeInterval = 550;
+        break;
+      case 5:
+        data.timeInterval = 400;
+        break;
+      case 6:
+        data.timeInterval = 200;
+        break;
+      case 7:
+        data.timeInterval = 100;
+        break;
     }
   }
 }
 
-function fail () {
+function fail() {
   clearInterval(TIME);
   maskScore.innerHTML = data.score;
   data.start = false;
@@ -543,7 +594,7 @@ function fail () {
   starButton.innerHTML = 'RESTART';
 }
 
-function initialization () {
+function initialization() {
   createFrame();
   data.start = true;
   data.timeInterval = 1000;
@@ -556,7 +607,7 @@ function initialization () {
   data.nextBox = new Array(2);
   data.clearAnimation = new Array(4);
   for (var i = 0, currentDivs = mainBox.getElementsByTagName('div'), ilen = currentDivs.length; i < ilen; i++) {
-    data.box[i]  = new Array(data.col);
+    data.box[i] = new Array(data.col);
     var currentDiv = currentDivs[i];
     for (var j = 0, currentSpans = currentDiv.getElementsByTagName('span'), jlen = currentSpans.length; j < jlen; j++) {
       data.box[i][j] = {};
@@ -566,7 +617,7 @@ function initialization () {
     }
   }
   for (var i = 0, currentDivs = nextBox.getElementsByTagName('div'), ilen = currentDivs.length; i < ilen; i++) {
-    data.nextBox[i]  = new Array(2);
+    data.nextBox[i] = new Array(2);
     var currentDiv = currentDivs[i];
     for (var j = 0, currentSpans = currentDiv.getElementsByTagName('span'), jlen = currentSpans.length; j < jlen; j++) {
       data.nextBox[i][j] = {};
@@ -579,12 +630,12 @@ function initialization () {
   scoreBox.getElementsByTagName('span')[0].innerHTML = PrefixInteger(data.score, 5);
 }
 
-function createFrame () {
+function createFrame() {
   createFrameContext(mainBox, data.row, data.col);
   createFrameContext(nextBox, 2, 4);
 }
 
-function createFrameContext (ele, rowValue, colValue) {
+function createFrameContext(ele, rowValue, colValue) {
   ele.innerHTML = '';
   var row = new Array(rowValue);
   for (var i = 0; i < rowValue; i++) {
