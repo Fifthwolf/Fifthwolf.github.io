f7789c279084f9f2ff4eb6b4ae8ab25e5127f635
Lei Xia <fifthwolf@qq.com>
2018-04-24 15:33:00 +0800

    优化代码

--- js/perpetualCalendar.js
+++ js/perpetualCalendar.js
@@ -5,18 +5,11 @@ var content = right.$('.content.0'),
   tableBody = table.$('tbody.0'),
   tdDays = tableBody.$('td'),
   temp = '';
+var data = {
+  MINYEAR: 2000,
+  MAXYEAR: 2049
+}
 var now = new Date();
-
-for (var i = 2000; i < 2050; i++) {
-  temp += '<option value="' + i + '">' + i + '</option>'
-}
-year.innerHTML = temp;
-temp = '';
-
-for (var i = 1; i <= 12; i++) {
-  temp += '<option value="' + i + '">' + i + '</option>'
-}
-month.innerHTML = temp;
 
 year.onchange = function() {
   dayList();
@@ -28,10 +21,11 @@ month.onchange = function() {
 
 window.onload = function() {
   delayedLoadingPublicPictures('../');
+  init();
   dayList(now);
-  var currentYear = getSelectOption(year, now.getFullYear());
-  var currentMonth = getSelectOption(month, now.getMonth() + 1);
-  var currentDay = getSelectDays(tdDays);
+  var currentYear = getSelectOption(year, now.getFullYear()),
+    currentMonth = getSelectOption(month, now.getMonth() + 1),
+    currentDay = getSelectDays(tdDays);
   currentYear.setAttribute('selected', 'true');
   currentMonth.setAttribute('selected', 'true');
   if (!judgeWidth()) {
@@ -57,57 +51,58 @@ addEvent(tableBody, 'mouseout', function(e) {
   }
 });
 
+function init() {
+  for (var i = data.MINYEAR; i <= data.MAXYEAR; i++) {
+    temp += '<option value="' + i + '">' + i + '</option>'
+  }
+  year.innerHTML = temp;
+  temp = '';
+
+  for (var i = 1; i <= 12; i++) {
+    temp += '<option value="' + i + '">' + i + '</option>'
+  }
+  month.innerHTML = temp;
+}
+
 //移动端添加滑动事件
 function touch(element) {
-  var startx;
-  var endx;
-
-  function _cons() {
-    if (startx > endx) {
-      changeTime(1);
-    } else {
-      changeTime(2);
-    }
-  }
+  var startx,
+    endx;
 
   addEvent(element, 'touchstart', function(e) {
     e.preventDefault();
     var touch = e.changedTouches;
     startx = touch[0].clientX;
-    starty = touch[0].clientY;
   });
 
   addEvent(element, 'touchend', function(e) {
     e.preventDefault();
     var touch = e.changedTouches;
     endx = touch[0].clientX;
-    endy = touch[0].clientY;
-    if (Math.abs(endx - startx) > 150) {
-      _cons();
+    if (Math.abs(endx - startx) > 40) {
+      changeTime(startx > endx ? 1 : 2);
     }
   });
 }
 
 function changeTime(change) {
-  var index = year.selectedIndex;
-  yearValue = year.options[index].value;
-  index = month.selectedIndex;
-  monthValue = month.options[index].value;
+  var yearValue = year.options[year.selectedIndex].value,
+    monthValue = month.options[month.selectedIndex].value;
   if (change == 1) {
     monthValue--;
     if (monthValue < 1) {
-      yearValue = Math.max((Number(yearValue) - 1), 2000);
+      yearValue = Math.max((Number(yearValue) - 1), data.MINYEAR);
       monthValue = 12;
-      if (yearValue == 2000) {
+      if (yearValue == data.MINYEAR) {
         monthValue = 1;
       }
     }
   } else if (change == 2) {
     monthValue++;
     if (monthValue > 12) {
-      yearValue = Math.min((Number(yearValue) + 1), 2049);
+      yearValue = Math.min((Number(yearValue) + 1), data.MAXYEAR);
       monthValue = 1;
-      if (yearValue == 2049) {
+      if (yearValue == data.MAXYEAR) {
         monthValue = 12;
       }
     }
@@ -120,10 +115,8 @@ function changeTime(change) {
 function dayList(date) {
   var yearValue, monthValue;
   if (!date) {
-    var index = year.selectedIndex;
-    yearValue = year.options[index].value;
-    index = month.selectedIndex;
-    monthValue = month.options[index].value;
+    yearValue = year.options[year.selectedIndex].value;
+    monthValue = month.options[month.selectedIndex].value;
     date = new Date(yearValue + '//' + monthValue + '//01');
   } else {
     yearValue = date.getFullYear();
@@ -195,13 +188,8 @@ function dayAttr(year, month) {
       days = 31;
       break;
     case 2:
-      if (Leap == 1) {
-        days = 29;
-        break;
-      } else {
-        days = 28;
-        break;
-      }
+      days = Leap === 1 ? 29 : 28;
+      break;
     default:
       days = 30;
       break;
@@ -214,7 +202,7 @@ function dayAttr(year, month) {
 
 //寻找对应值的元素
 function getSelectOption(parent, value) {
-  var options = parent.getElementsByTagName("option");
+  var options = parent.$('option');
   for (var i = 0; i < options.length; i++) {
     if (options[i].value == value) {
       return options[i];
@@ -233,6 +221,6 @@ function getSelectDays(parent, value) {
 }
 
 function useContentHeight() {
-  var trs = content.getElementsByTagName('tr');
+  var trs = content.$('tr');
   content.style.height = trs.length * 50 + 65 + 'px';
 }
