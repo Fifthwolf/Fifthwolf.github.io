62f884ef2a38e876c5d7ef4d9ca2e88b1c8be38d
Lei Xia <fifthwolf@qq.com>
2018-04-25 11:25:31 +0800

    update

--- js/pictureClipping.js
+++ js/pictureClipping.js
@@ -9,8 +9,8 @@ document.onselectstart = new Function('event.returnValue=false;');
 
 window.onload = function() {
   delayedLoadingPublicPictures('../');
-  var show = document.getElementById("show");
-  var showPic = document.createElement("img");
+  var show = $('#show');
+  var showPic = document.createElement('img');
   showPic.src = img2.src;
   showPic.id = "showPi";
   show.appendChild(showPic);
@@ -82,50 +82,46 @@ function move(index, limitX, limitY, minLength, element) {
 
 //左边拖动
 function leftMove(e, limitX, minLength) {
-  var x = e.clientX;
-  x = moveLimit(x, getPosition(img2).left, limitX - minLength);
-  var width = mainDiv.offsetWidth - 2;
-  var mainX = getPosition(mainDiv).left;
-  var addWidth = mainX - x;
+  var x = moveLimit(e.clientX, getPosition(img2).left, limitX - minLength),
+    width = mainDiv.offsetWidth - 2,
+    mainX = getPosition(mainDiv).left,
+    addWidth = mainX - x;
   mainDiv.style.width = (width + addWidth) + "px";
   mainDiv.style.left = mainDiv.offsetLeft - mainX + x + "px";
 }
 
 //右边拖动
 function rightMove(e, minLength) {
-  var x = e.clientX;
-  x = moveLimit(x, getPosition(mainDiv).left + minLength, getPosition(img2).left + img2.offsetWidth);
-  var width = mainDiv.offsetWidth - 2;
-  var mainX = getPosition(mainDiv).left;
-  var addWidth = x - width - mainX;
+  var x = moveLimit(e.clientX, getPosition(mainDiv).left + minLength, getPosition(img2).left + img2.offsetWidth),
+    width = mainDiv.offsetWidth - 2,
+    mainX = getPosition(mainDiv).left,
+    addWidth = x - width - mainX;
   mainDiv.style.width = (width + addWidth) + "px";
 }
 
 //上边拖动
 function upMove(e, limitY, minLength) {
-  var y = e.clientY;
-  y = moveLimit(y, getPosition(img2).top, limitY - minLength);
-  var height = mainDiv.offsetHeight - 2;
-  var mainY = getPosition(mainDiv).top;
-  var addHeight = mainY - y;
+  var y = moveLimit(e.clientY, getPosition(img2).top, limitY - minLength),
+    height = mainDiv.offsetHeight - 2,
+    mainY = getPosition(mainDiv).top,
+    addHeight = mainY - y;
   mainDiv.style.height = (height + addHeight) + "px";
   mainDiv.style.top = mainDiv.offsetTop - mainY + y + "px";
 }
 
 //下边拖动
 function downMove(e, minLength) {
-  var y = e.clientY;
-  y = moveLimit(y, getPosition(mainDiv).top + minLength, getPosition(img2).top + img2.offsetHeight);
-  var height = mainDiv.offsetHeight - 2;
-  var mainY = getPosition(mainDiv).top;
-  var addHeight = y - height - mainY;
+  var y = moveLimit(e.clientY, getPosition(mainDiv).top + minLength, getPosition(img2).top + img2.offsetHeight),
+    height = mainDiv.offsetHeight - 2,
+    mainY = getPosition(mainDiv).top,
+    addHeight = y - height - mainY;
   mainDiv.style.height = (height + addHeight) + "px";
 }
 
 //移动
 function allMove(e, disX, disY, element) {
-  var addX = e.clientX - disX - getPosition(boxDiv).left;
-  var addY = e.clientY - disY - getPosition(boxDiv).top;
+  var addX = e.clientX - disX - getPosition(boxDiv).left,
+    addY = e.clientY - disY - getPosition(boxDiv).top;
   mainDiv.style.left = moveLimit(addX, element.offsetLeft, element.offsetWidth - mainDiv.offsetWidth + element.offsetLeft) + "px";
   mainDiv.style.top = moveLimit(addY, element.offsetTop, element.offsetHeight - mainDiv.offsetHeight + element.offsetTop) + "px";
 }
@@ -138,9 +134,9 @@ function moveLimit(n, min, max) {
 
 //获取元素的绝对位置
 function getPosition(node) {
-  var left = node.offsetLeft;
-  var top = node.offsetTop;
-  current = node.offsetParent;
+  var left = node.offsetLeft,
+    top = node.offsetTop,
+    current = node.offsetParent;
   while (current) {
     left += current.offsetLeft;
     top += current.offsetTop;
@@ -170,7 +166,7 @@ function setChoice(element) {
 
 //预览图
 function preview(view) {
-  var previewImg = document.getElementById("showPi");
+  var previewImg = $("#showPi");
   previewImg.style.top = -view.top + "px";
   previewImg.style.left = -view.left + "px";
   previewImg.style.clip = "rect(" + view.top + "px," + view.right + "px," + view.bottom + "px," + view.left + "px)";
